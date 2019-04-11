ebd13e692964be6b9b286d7fe3bcf469ee1d6bc1
Eric Castronovo <ecastronovo@scu.edu>
2019-04-11 13:41:46 -0700

    prototyping table view cells for main page.

---
+++ RunneriOSFrontend/Controller/TaskTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TaskTableViewCell.swift
+//  RunneriOSFrontend
+//
+//  Created by Eric Castronovo on 4/11/19.
+//  Copyright © 2019 Eric Castronovo. All rights reserved.
+//
+
+import UIKit
+
+class TaskTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
